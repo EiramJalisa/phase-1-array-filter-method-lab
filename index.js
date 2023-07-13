39ad5e7df1139c8b189a34e822369eb63d8f3fc3
Jalisa Williams <jcarter0526@gmail.com>
2023-07-13 12:31:40 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching (takenArray, passedString) {
+  return takenArray.filter(bobby => bobby.toLowerCase() === passedString.toLowerCase());
+}
+
+function fuzzyMatch(takenArrayV2, passedString){
+  return takenArrayV2.filter(function(stringPiece) {
+      if (stringPiece.slice(0, passedString.length) === passedString){
+          return true;
+  }
+  })
+}
+
+function matchName(driversAndCities, passedString){
+  return driversAndCities.filter(driverName => driverName.name === passedString)
+}
